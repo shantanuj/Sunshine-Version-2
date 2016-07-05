a9cd567f8a5dd6a93f7c4c7a9ffc6685f1ec9af1
shantanuj <shantanu12jswl@gmail.com>
2016-07-05 16:33:22 +0800

    Added a list view, with fake data. Onwards to the cloud!

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,6 +25,8 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+
     }
 
     @Override
@@ -57,6 +63,20 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ArrayList<String> weekForecast;
+            weekForecast=new ArrayList<String>();
+            weekForecast.add("Today - Sunny - 88/63");
+            weekForecast.add("Tomorrow - Foggy - 70/46");
+            weekForecast.add("Weds - Cloudy - 72/63");
+            weekForecast.add("Thurs - Rainy - 64/51");
+            weekForecast.add("Fri - Foggy - 70/46");
+            weekForecast.add("Sat - HELP");
+            weekForecast.add("Sun - Sunny - 80/68");
+            ArrayAdapter<String> ad=new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+
+            ListView l1=(ListView)getActivity().findViewById(R.id.listview_forecast);
+            l1.setAdapter(ad);
+
             return rootView;
         }
     }
